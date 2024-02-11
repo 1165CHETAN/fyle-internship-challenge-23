386768169434252154a1bfa3a3349eef34ff2f80
Chetan Chavan <111604779+1165CHETAN@users.noreply.github.com>
2024-02-11 14:28:11 +0530

    added repository feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,92 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
+import { HttpClient } from '@angular/common/http'; // Import HttpClient module
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+  user: any;
+  userNotFound: boolean = false;
+  searchUsername: string = '';
+  repositories: any[] = [];
+  Object = Object;
+  isLoadingUser: boolean = false;
+  dataLoaded: boolean = false;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  skeletonArray: any[] = Array(10).fill(0);
+
   constructor(
-    private apiService: ApiService
+    private apiService: ApiService,
+    private http: HttpClient // Inject HttpClient module
   ) {}
 
-  ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+  ngOnInit() {}
+
+  searchUser(username: string) {
+    this.isLoadingUser = true;
+    this.userNotFound = false;
+    this.repositories = [];
+
+    // Fetch user data
+    this.http.get<any>('https://api.github.com/users/' + username).subscribe(
+      (user: any) => {
+        this.user = user;
+        this.isLoadingUser = false;
+        this.dataLoaded = true;
+
+        // Fetch repositories data
+        this.http.get<any[]>('https://api.github.com/users/' + username + '/repos').subscribe(
+          (repos: any[]) => {
+            this.repositories = repos;
+          },
+          (error) => {
+            console.error('Error fetching repositories:', error);
+          }
+        );
+      },
+      (error) => {
+        console.error('Error fetching user:', error);
+        this.userNotFound = true;
+        this.isLoadingUser = false;
+      }
+    );
+  }
+
+  formatFollowersCount(count: number): string {
+    if (count > 1000) {
+      return (count / 1000).toFixed(1) + 'k';
+    } else {
+      return count.toString();
+    }
+  }
+
+  onPageSizeChange() {
+    this.currentPage = 1;
+  }
+
+  getPageNumbers(): number[] {
+    const pageCount = Math.ceil(this.repositories.length / this.pageSize);
+    return Array(pageCount).fill(0).map((x, i) => i + 1);
+  }
+
+  goToPage(pageNumber: number) {
+    this.currentPage = pageNumber;
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    const pageCount = Math.ceil(this.repositories.length / this.pageSize);
+    if (this.currentPage < pageCount) {
+      this.currentPage++;
+    }
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,50 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { forkJoin, Observable, throwError } from 'rxjs';
+import { catchError, map, mergeMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  private githubToken = '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'; // Replace with your personal access token
 
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  constructor(private httpClient: HttpClient) {}
+
+  getUser(githubUsername: string): Observable<any> {
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`).pipe(
+      catchError(error => {
+        console.error('Error:', error);
+        return throwError(error);
+      })
+    );
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getRepos(githubUsername: string): Observable<any[]> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.githubToken}`
+    });
+
+    return this.httpClient.get<any[]>(`https://api.github.com/users/${githubUsername}/repos`, { headers }).pipe(
+      catchError(error => {
+        console.error('Error fetching repositories:', error);
+        return throwError(error);
+      }),
+      mergeMap((repos: any[]) => {
+        const observables: Observable<any>[] = repos.map(repo => {
+          return this.httpClient.get<any>(repo.languages_url, { headers }).pipe(
+            catchError(error => {
+              console.error('Error fetching languages:', error);
+              return throwError(error);
+            }),
+            map((languages: any) => {
+              return { ...repo, languages };
+            })
+          );
+        });
+        return forkJoin(observables) as Observable<any[]>;
+      })
+    );
+  }
 }
